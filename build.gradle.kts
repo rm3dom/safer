4f1274667438d796bc46f018d32830b6fe854daf
ruans <rm3dom@gmail.com>
2025-05-30 22:53:19 +1000

    Add gradle plugin publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 val buildTool = stringProperty("safer.buildTool", "")
+val saferVersion = stringProperty("safer.version", "")
+val enablePublishing = boolProperty("safer.publish", false)
 
 require(buildTool in listOf("maven", "gradle")) {
     "safer.buildTool must be one of maven, gradle, but was '$buildTool' instead."
@@ -14,5 +16,13 @@ tasks.create("gradle-dev-publish") {
 
 tasks.create("maven-dev-publish") {
     dependsOn(":safer-maven-plugin:publishToMavenLocal")
+}
+
+tasks.create("gradle-plugin-publish") {
+    if(enablePublishing) {
+        dependsOn(":safer-gradle-plugin:publish")
+        if(!saferVersion.contains("SNAPSHOT", true))
+            dependsOn(":safer-gradle-plugin:publishPlugins")
+    }
 }
 
--- safer-gradle-plugin/build.gradle.kts
+++ safer-gradle-plugin/build.gradle.kts
@@ -10,13 +10,18 @@ dependencies {
     compileOnly(libs.kotlin.gradle.plugin.api)
 }
 
+ext["gradle.publish.key"] = stringProperty("GRADLE_KEY", "bad")
+ext["gradle.publish.secret"] = stringProperty("GRADLE_SECRET", "bad")
+
 gradlePlugin {
+
     vcsUrl = "https://github.com/rm3dom/safer.git"
     website = "https://github.com/rm3dom/safer"
     plugins {
         create("SaferGradlePlugin") {
             id = "com.swiftleap.safer"
             displayName = "Safer Gradle plugin"
+            version = project.version as String
             description = project.description
             implementationClass = "com.swiftleap.safer.SaferGradlePlugin"
             tags = listOf(
